75e2458884c62867745b60e9f3f50cc5ee295822
mu009009 <amoi0523@gmail.com>
2016-09-02 16:19:50 -0400

    clear

--- Script/GlobalControl.js
+++
@@ -1,4 +0,0 @@
-// Time for Transition Animation setting
-var ShorDurationTime = 200;
-var MiddleDurationTime = 500;
-var LongDurationTime = 1000;
--- Script/TransForm.js
+++
@@ -1,6 +0,0 @@
-function ValuewithPxStrtoNumValue(str)
-{
-	var StrOriginal = str;
-	var NoPxNum = StrOriginal.substring(0,(StrOriginal.length-2));
-	return NoPxNum;
-}
